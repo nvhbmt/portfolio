1c86fa2f477a8926ce2f2d1ed02b8db7309a1374
Nguyen Hoang <cunpoi1357@gmail.com>
2024-08-18 22:33:57 +0700

    chore: Add site and base configuration in Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  site: 'https://nvhbmt.github.io',
+  base: 'portfolio',
 });
